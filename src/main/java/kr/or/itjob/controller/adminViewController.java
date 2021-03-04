b80ccef5f6e2be65acd2c73fa389341607a529be
bsjin <bsjin7231@gmail.com>
2021-03-04 11:11:27 +0900

    final merge

--- src/main/java/kr/or/itjob/controller/adminViewController.java
+++ src/main/java/kr/or/itjob/controller/adminViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("adminview")
 @Controller
 public class adminViewController {
-	
+
 	@RequestMapping("login")
 	public String loginView() {
 		return "/adminlogin/adminlogin";
@@ -16,39 +16,44 @@ public class adminViewController {
 	public String advancedView() {
 		return "/admin/datatable";
 	}
-	
+
 	@RequestMapping("admin_mypage")
 	public String user_mypageView() {
 		return "/admin/mypage";
-		
+
 	}
-	
+
 	@RequestMapping("codingTest")
 	public String codingTestView() {
 		return "/admin/codingTest";
 	}
-	
+
 	@RequestMapping("calendarView")
 	public String calendarView() {
 		return "/admin/calendar";
 	}
+
 	@RequestMapping("faq")
 	public String faqView() {
 		return "/admin/faq";
 	}
+
 //test03
 	@RequestMapping("mypage123123")
 	public String mypageView123123() {
+		return "/admin/faq";
+	}
 
-	//modify 
+	// modify
 	@RequestMapping("mypage")
 	public String mypageView() {
 		return "/admin/mypagex";
 	}
-	//added
+
+	// added
 	@RequestMapping("mypage2")
 	public String mypageView2() {
 		return "/admin/mypage";
 	}
-	
+
 }
